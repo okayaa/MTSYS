17c1391f49852359c3fcc9ef6d631ebd5c07ae3a
Akifumi Okayama <akifumi.okayama@akane.waseda.jp>
2017-07-28 21:40:27 +0900

    Respond to comments for the 1st resubmission
    Import stats package

--- R/utils.R
+++ R/utils.R
@@ -1,103 +1,104 @@
-#' Function to predict a diagnosis for a family of Mahalanobis-Taguchi (MT) 
+#' Function to predict a diagnosis for a family of Mahalanobis-Taguchi (MT)
 #'   methods
-#' 
-#' \code{diagnosis} is a generic function. For details, see 
-#'   \code{\link{diagnosis.MT}}, \code{\link{diagnosis.MTA}}, 
-#'   \code{\link{diagnosis.RT}} or \code{\link{general_diagnosis.MT}}.   
-#'                 
+#'
+#' \code{diagnosis} is a generic function. For details, see
+#'   \code{\link{diagnosis.MT}}, \code{\link{diagnosis.MTA}},
+#'   \code{\link{diagnosis.RT}} or \code{\link{general_diagnosis.MT}}.
+#'
 #' @param unit_space Object generated as a unit space.
-#' @param newdata Matrix with n rows (samples) and p columns (variables). The 
-#'                  data are used to calculate the desired distances from the 
-#'                  unit space. All data should be continuous values and should 
+#' @param newdata Matrix with n rows (samples) and p columns (variables). The
+#'                  data are used to calculate the desired distances from the
+#'                  unit space. All data should be continuous values and should
 #'                  not have missing values.
-#' @param threshold Numeric specifying the threshold value to classify each 
-#'                    sample into positive (\code{TRUE}) or negative 
+#' @param threshold Numeric specifying the threshold value to classify each
+#'                    sample into positive (\code{TRUE}) or negative
 #'                    (\code{FALSE}).
-#' @param includes_transformed_newdata If \code{TRUE}, then the transformed data 
-#'                                       for \code{newdata} are included in a 
+#' @param includes_transformed_newdata If \code{TRUE}, then the transformed data
+#'                                       for \code{newdata} are included in a
 #'                                       return object.
-#' 
+#'
 #' @return A list containing the following components is returned.
-#' 
-#'  \item{distance}{Vector with length n. Distances from the unit space to each 
+#'
+#'  \item{distance}{Vector with length n. Distances from the unit space to each
 #'                   sample.}
-#'  \item{le_threshold}{Vector with length n. Logical values indicating the 
-#'                       distance of each sample is less than or equal to the 
+#'  \item{le_threshold}{Vector with length n. Logical values indicating the
+#'                       distance of each sample is less than or equal to the
 #'                       threhold value (\code{TRUE}) or not (\code{FALSE}).}
-#'  \item{threshold}{Numeric value to classify the sample into positive or 
+#'  \item{threshold}{Numeric value to classify the sample into positive or
 #'                    negative.}
 #'  \item{unit_space}{Object passed by \code{unit_space}.}
 #'  \item{n}{The number of samples for \code{newdata}.}
 #'  \item{q}{The number of variables after the data transformation.}
-#'  \item{x}{If \code{includes_transformed_newdata} is \code{TRUE}, then the 
+#'  \item{x}{If \code{includes_transformed_newdata} is \code{TRUE}, then the
 #'            transformed data for \code{newdata} are included.}
-#'    
-#' @seealso \code{\link{diagnosis.MT}}, \code{\link{diagnosis.MTA}}, and 
+#'
+#' @seealso \code{\link{diagnosis.MT}}, \code{\link{diagnosis.MTA}}, and
 #'            \code{\link{diagnosis.RT}}
-#' 
+#'
 #' @export
-diagnosis <- function(unit_space, 
-                      newdata, 
-                      threshold, 
+diagnosis <- function(unit_space,
+                      newdata,
+                      threshold,
                       includes_transformed_newdata) {
-  
+
   UseMethod("diagnosis", unit_space)
-  
+
 }
 
 #' Function to predict a forecasting for a family of Taguchi (T) methods
-#' 
-#' \code{forecasting} is a generic function. For details, see 
-#'   \code{\link{forecasting.T1}}, \code{\link{forecasting.Ta}}, 
-#'   \code{\link{forecasting.Tb}} or \code{\link{general_forecasting.T}}.   
-#'                 
-#' @param model Object generated as a model. 
-#' @param newdata Matrix with n rows (samples) and p columns (variables). The 
-#'                  Data to be estimated. All data should be continuous values 
-#'                  and should not have missing values.  
-#' @param includes_transformed_newdata If \code{TRUE}, then the transformed data 
-#'                                       for \code{newdata} are included in a 
+#'
+#' \code{forecasting} is a generic function. For details, see
+#'   \code{\link{forecasting.T1}}, \code{\link{forecasting.Ta}},
+#'   \code{\link{forecasting.Tb}} or \code{\link{general_forecasting.T}}.
+#'
+#' @param model Object generated as a model.
+#' @param newdata Matrix with n rows (samples) and p columns (variables). The
+#'                  Data to be estimated. All data should be continuous values
+#'                  and should not have missing values.
+#' @param includes_transformed_newdata If \code{TRUE}, then the transformed data
+#'                                       for \code{newdata} are included in a
 #'                                       return object.
-#' 
+#'
 #' @return A list containing the following components is returned.
-#' 
-#'  \item{M_hat}{Vector with length n. The estimated values of the dependent 
+#'
+#'  \item{M_hat}{Vector with length n. The estimated values of the dependent
 #'                variable after the data trasformation.}
-#'  \item{y_hat}{Vector with length n. The estimated values after the inverse 
+#'  \item{y_hat}{Vector with length n. The estimated values after the inverse
 #'                transformation from \code{M_hat}.}
 #'  \item{model}{Object passed by \code{model}.}
 #'  \item{n}{The number of samples for \code{newdata}.}
 #'  \item{q}{The number of variables after the data transformation.}
-#'  \item{X}{If \code{includes_transformed_newdata} is \code{TRUE}, then the 
+#'  \item{X}{If \code{includes_transformed_newdata} is \code{TRUE}, then the
 #'            transformed data for \code{newdata} are included.}
-#' 
-#' @seealso \code{\link{forecasting.T1}}, \code{\link{forecasting.Ta}}, and 
+#'
+#' @seealso \code{\link{forecasting.T1}}, \code{\link{forecasting.Ta}}, and
 #'            \code{\link{forecasting.Tb}}
-#'            
+#'
 #' @export
 forecasting <- function(model, newdata, includes_transformed_newdata) {
   UseMethod("forecasting", model)
 }
 
-check_data <- function(data) {  
+#' @importFrom stats complete.cases
+check_data <- function(data) {
   if (!all(complete.cases(data))) {
-    stop(paste("Some missing values are including in data.", 
+    stop(paste("Some missing values are including in data.",
                "Data without missing are needed."))
   }
-  
+
   if (sum(!is.numeric(as.matrix(data))) != 0) {
     warning("Data include some values which are not numeric.")
   }
-  
+
 }
 
 # Used at a test of generates_dimensionality_reduction_function
 restores_Ys <- function(object) {
-  
+
   center <- attr(object, "scaled:center")
-  
+
   attr(object, "dimname") <- NULL
   attr(object, "scaled:center") <- NULL
-  
+
   return(as.data.frame(t(t(object) + center)))
 }
--- cran-comments.md
+++ cran-comments.md
@@ -1,6 +1,10 @@
+## Resubmission
+This is a resubmission. In this version I have modified Imports field in the 
+DESCRIPTION file.
+
 ## Test environments
 * local OS X install, R 3.4.1
-* local windows 10 install, R 3.4.1
+* win-builder (devel and release)
 
 ## R CMD check results
 0 errors | 0 warnings | 0 notes
